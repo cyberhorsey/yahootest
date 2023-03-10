89fdefa3a2417d50acff6bb1e4b163f102d6e28e
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 12:38:18 -0800

    alias?

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
-import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), nodePolyfills()],
   define: { global: "window" },
+  resolve: {
+    alias: {
+      buffer: "buffer/",
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
