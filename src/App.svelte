22b7440d0055bdb78f2c8b4bdaafec98f705bddf
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 12:51:52 -0800

    .

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import NftView from "./components/NFTView.svelte";
   import account from "./store/account";
 
-  import { Buffer } from "buffer";
+  import { Buffer } from "buffer/";
   import Process from "process";
   import Home from "./components/Home.svelte";
   globalThis.process = Process;
