9678dc8b8b75fbe4e90e01269c506edf77d71017
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 12:54:50 -0800

    .

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,7 @@
   import NftView from "./components/NFTView.svelte";
   import account from "./store/account";
 
-  import { Buffer } from "buffer/";
-  import Process from "process";
   import Home from "./components/Home.svelte";
-  globalThis.process = Process;
-  globalThis.Buffer = Buffer;
-  window.Buffer = window.Buffer || Buffer;
 </script>
 
 <Navbar />
--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,19 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
 import inject from "@rollup/plugin-inject";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), nodePolyfills()],
-  define: { global: "window" },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
     rollupOptions: {
-      plugins: [
-        inject({
-          include: ["node_modules/@loopring-web/**", "node_modules/@imtbl/**"],
-          modules: { Buffer: ["buffer", "Buffer"] },
-        }),
-      ],
+      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
     },
   },
 });
