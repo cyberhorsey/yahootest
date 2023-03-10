4e78a4adb3bf1b0510964efa35dd72d878daeb0c
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 13:03:15 -0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import requireTransform from "vite-plugin-require-transform";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), nodePolyfills(), requireTransform()],
+  plugins: [svelte(), nodePolyfills(), requireTransform({})],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
