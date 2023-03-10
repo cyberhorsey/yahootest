5d25eae0c5b8d922601e8028cdf40d7e7f29dc03
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 13:18:27 -0800

    .

--- src/utils/fetchNftsByAddress.ts
+++ src/utils/fetchNftsByAddress.ts
@@ -2,19 +2,17 @@ import type { Account } from "../domain/account";
 import { AccountType } from "../domain/account";
 import type { NFT } from "../domain/nft";
 import fetchEthereumNfts from "./fetchEthereumNfts";
-import fetchImxNfts from "./fetchImxNfts";
 import fetchLoopringNfts from "./fetchLoopringNfts";
 
 async function fetchNftsByAddress(account: Account): Promise<NFT[]> {
   if (account.accountType === AccountType.Ethereum) {
-    const imxNfts: NFT[] = await fetchImxNfts(account.address);
     const ethereumNfts: NFT[] = await fetchEthereumNfts(
       account.address,
       import.meta.env.VITE_ALCHEMY_API_KEY
     );
     const loopringNfts: NFT[] = await fetchLoopringNfts(account.address);
 
-    return imxNfts.concat(ethereumNfts, loopringNfts);
+    return ethereumNfts.concat(loopringNfts);
   } else {
     return [];
   }
