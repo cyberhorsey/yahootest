70c7b9f34229d13981f696e7646a72f9a2a63f05
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 12:25:02 -0800

    try this

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.css";
+import App from "./App.svelte";
+
+import { Buffer } from "buffer";
+import Process from "process";
+globalThis.process = Process;
+globalThis.Buffer = Buffer;
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 const app = new App({
-  target: document.getElementById('app')
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
