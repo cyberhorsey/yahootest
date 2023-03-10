1fcdb756b0f0bb19f17d7439bfbaf05628ef77e1
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 13:07:44 -0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
 import inject from "@rollup/plugin-inject";
 import * as path from "path";
-import requireTransform from "vite-plugin-require-transform";
-
+import legacy from "@vitejs/plugin-legacy";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), nodePolyfills(), requireTransform({})],
+  plugins: [
+    svelte(),
+    nodePolyfills(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
