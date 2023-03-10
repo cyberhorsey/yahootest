a8041b225359e614f0a9dff0b20e81a897c8d5cb
Jeffery Walsh <cyberhorsey@gmail.com>
2023-03-10 12:37:42 -0800

    try this

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
-import GlobalsPolyfills from "@esbuild-plugins/node-globals-polyfill";
+import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,12 +14,6 @@ export default defineConfig({
         global: "globalThis",
       },
       // Enable esbuild polyfill plugins
-      plugins: [
-        GlobalsPolyfills({
-          process: true,
-          buffer: true,
-        }),
-      ],
     },
   },
 });
